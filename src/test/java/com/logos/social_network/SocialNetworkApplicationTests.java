3e5ad75b9ee441752da5cd63618d78c9a050dc77
YuriiSolodjuk <57952100+Yura07@users.noreply.github.com>
2020-02-12 22:26:19 +0200

    Initial commit

---
+++ src/test/java/com/logos/social_network/SocialNetworkApplicationTests.java
@@ -0,0 +1,13 @@
+package com.logos.social_network;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SocialNetworkApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
